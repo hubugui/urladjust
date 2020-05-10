6af0d3aa531c369b66a37b6572fccc58816c913d
hubugui <hubugui@gmail.com>
2020-05-10 22:52:17 +0800

    pass testcase

--- main.c
+++ main.c
@@ -1,40 +1,33 @@
 #include <stdio.h>
 #include <string.h>
 
-static char *testcases[][2] = {
-    {"/index.m3u8", "http://127.0.0.1/index.m3u8"},
-    {"/test/../../index0.ts", "http://127.0.0.1/index0.ts"},
-    {"/test/../helloworld/../index1.ts", "http://127.0.0.1/index1.ts"},
-    {"/test/../../helloworld/xiaohei/../index2.ts"
-        , "http://127.0.0.1/helloworld/index2.ts"},
-    {"/test/helloworld/xiaohei/../index3.ts"
-        , "http://127.0.0.1/test/helloworld/index3.ts"},
-    {"/../test/helloworld/xiaohei/../index4.ts"
-        , "http://127.0.0.1/test/helloworld/index4.ts"},
-    {"/./test/helloworld/xiaohei/../index5.ts"
-        , "http://127.0.0.1/./test/helloworld/index5.ts"},
-    {"/test/helloworld/xiaohei/../index6.ts"
-        , "http://127.0.0.1/test/helloworld/index6.ts"},
-    {"/../../test/helloworld/xiaohei/../../../../../../index7.ts"
-        , "http://127.0.0.1/index7.ts"},
-};
+int strchr_reverse(const char *str, int begin, char c)
+{
+    int ret = -1;
+
+    while (begin > -1) {
+        if (str[begin] == c)
+            return begin;
+        begin--;
+    }
+    return ret;
+}
 
 char *url_adjust(char *url, const char *prefix, char *output)
 {
     char *ret = output;
+    char pre_is_dot = 0, dotdot = 0;
     int i = 0, op_point = 0;
-    char dotdot = 0;
-    char pre_is_dot = 0;
+    size_t prefix_len;
 
     if (!url || !prefix || !output)
         return NULL;
 
-    /**
-     * input:   "/test/../../helloworld/xiaohei/../index2.ts"
-     * output:  "/helloworld/index2.ts"},
-     * input:   "/../../test/helloworld/xiaohei/../../../../../../index7.ts""
-     * output:  "/index7.ts"},
-     */
+    /* copy prefix and move output */
+    prefix_len = strlen(prefix);
+    strcpy(output, prefix);
+    output += prefix_len;
+
     while (url[i] != '\0') {
         if (url[i] == '.') {
             if (pre_is_dot) {
@@ -47,10 +40,12 @@ char *url_adjust(char *url, const char *prefix, char *output)
         output[op_point++] = url[i];
 
         if (dotdot) {
-            op_point = 0;
+            if (op_point >= 4)
+                op_point = strchr_reverse(output, op_point - 4, '/');
+            else
+                op_point = 0;
             dotdot = 0;
         }
-
         i++;
     }
 
@@ -58,23 +53,47 @@ char *url_adjust(char *url, const char *prefix, char *output)
     return ret;
 }
 
-int main(int argc, char **argv)
+static int _test_url_adjust(void)
 {
+    static char *testcases[][2] = {
+        {"/index.m3u8", "http://127.0.0.1/index.m3u8"},
+        {"/test/../../index0.ts", "http://127.0.0.1/index0.ts"},
+        {"/test/../helloworld/../index1.ts", "http://127.0.0.1/index1.ts"},
+        {"/test/../../helloworld/xiaohei/../index2.ts"
+            , "http://127.0.0.1/helloworld/index2.ts"},
+        {"/test/helloworld/xiaohei/../index3.ts"
+            , "http://127.0.0.1/test/helloworld/index3.ts"},
+        {"/../test/helloworld/xiaohei/../index4.ts"
+            , "http://127.0.0.1/test/helloworld/index4.ts"},
+        {"/./test/helloworld/xiaohei/../index5.ts"
+            , "http://127.0.0.1/./test/helloworld/index5.ts"},
+        {"/test/helloworld/xiaohei/../index6.ts"
+            , "http://127.0.0.1/test/helloworld/index6.ts"},
+        {"/../../test/helloworld/xiaohei/../../../../../../index7.ts"
+            , "http://127.0.0.1/index7.ts"},
+    };
     int ret = 0;
     const char *prefix = "http://127.0.0.1";
-    char buffer[4096] = {0};
+    char buffer[8192] = {0};
 
     for (int i = 0; i < sizeof(testcases) / (2 * sizeof(char *)); i++) {
         char *output = url_adjust(testcases[i][0], prefix, buffer);
 
         if (output && strcmp(output, testcases[i][1]) == 0)
-            printf("%02d pass, input=\"%s\", output=\"%s\"\n"
+            printf("%02d pass, input=\"%s\", \n\t output=\"%s\"\n"
                     , i, testcases[i][0], testcases[i][1]);
-        else
+        else {
+            ret = -1;
             printf("%02d fail, input=\"%s\", output=\"%s\", answer=\"%s\"\n"
                     , i, testcases[i][0], output ? output : "NULL"
                     , testcases[i][1]);
+        }
     }
     
     return ret;
+}
+
+int main(int argc, char **argv)
+{
+    return _test_url_adjust();
 }
